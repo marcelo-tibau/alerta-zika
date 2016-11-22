6add5c0b4b281e6bed053fec295fc9d9698eef6d
Marcelo Tibau <marcelo@tibau.org>
2016-11-22 12:21:20 -0200

    Add files via upload

--- alerta_zika.R
+++ alerta_zika.R
@@ -44,8 +44,96 @@ dim(testing)
 # check if the first column will interfer with ML Algorithms
 testtraining <- training[c(-1)]
 
+# Codes to clean the variables with more than 60% NAs at the training set
+temp_train <- training
+for (i in 1:length(training)) {
+  if(sum(is.na(training[,i]))/nrow(training)>=0.6) {
+    for (j in 1:length(temp_train)) {
+      if(length(grep(names(training[i]), names(temp_train)[j]))==1){
+        temp_train <- temp_train[,-j]
+      }
+      
+    }
+  }
+  
+}
+
+dim(temp_train)
+
+training <- temp_train
+rm(temp_train)
+
+# Codes to perform the same cleaning at the testing dataset:
+temp_test <- testing
+for (i in 1:length(testing)) {
+  if(sum(is.na(testing[,i]))/nrow(testing)>=0.6) {
+    for (j in 1:length(temp_test)) {
+      if(length(grep(names(testing[i]), names(temp_test)[j]))==1) {
+        temp_test <- temp_test[,-j]
+      }
+      
+    }
+  }
+  
+}
+
+dim(temp_test)
+
+testing <- temp_test
+rm(temp_test)
+
+# I will test 3 machine learning algorithms to build predictive models. One model, using a random forest ("rf"), the other, using decision trees and a third, using boosted trees ("gbm").
+# Then, I will perform cross-validations to predict the outcomes and check the accuracy of each model at the testing set.
+
+# Random Forest Algorithm -> so far it didn't work. I could not get an accuracy response.
+# Modelling with Random Forests for Survival, Regression and Classification (RF-SRC) package
+library('randomForestSRC')
+
+# Set seed for reproducibility purposes:
+set.seed(14000)
+
+# Omiting NA
+training_na <- na.omit(training)
+testing_na <- na.omit(testing)
+
+# Rip off the first column to prevent it interfers with ML Algorithms. 
+testtraining <- training_na[c(-1)]
+testtesting <- testing_na[c(-1)]
+
+# Fitting the model with the variables ID_BAIRRO and NU_CEP. I tried to fit with all variables but it didn't work.
+
+fit1 <- rfsrc(ID_BAIRRO~NU_CEP, data = testtraining, statistics=TRUE)
+
+# Cross validating the model:
+predictFit1 <- predict.rfsrc(fit1, testtesting, type = "class", statistics=TRUE)
+  
+accuracy_fit1 <- stat.split.rfsrc(predictFit1)
+
+# To check the accuracy:
+accuracy_fit1 <- confusionMatrix(predictFit1, testtesting)
 
 
+# Modelling with randomForest package
+
+set.seed(14000)
+modelFitRF <- randomForest(ID_BAIRRO~NU_CEP, data = testtraining)
+
+# Cross validating the model:
+predictFitRF <- predict(modelFitRF, testtesting, type = "class")
+
+# To check the accuracy:
+
+accuracy_FitRF <- confusionMatrix(pred, testtesting$ID_BAIRRO)
+accuracy_FitRF
+
+pred <- format(round(predict(modelFitRF,testtesting)))
+plot(pred)
+
+acc <- table(factor(pred, levels=min(testtesting):max(testtesting)),factor(testtesting, levels=min(testtesting):max(testtesting)))
+acc <- table(factor(pred), levels(testtesting), factor(testtesting))
+sort.list(pred)
+
+plot(predictFitRF)
 # In createDataPartition(y = data_zika_part$NM_DISEASE, p = 0.7, list = FALSE) :
 # Some classes have no records ( Chikungunya, Dengue ) and these will be ignored
 
