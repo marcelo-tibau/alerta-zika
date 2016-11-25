4d3dfbf8bc6247f080c06643aa3577b0a9e2c695
Marcelo Tibau <marcelo@tibau.org>
2016-11-25 21:33:53 -0200

    Add files via upload

--- zika_script3.R
+++ zika_script3.R
@@ -8,13 +8,21 @@ library("RColorBrewer")
 library("NbClust")
 library("geosphere")
 
+# Partitioning
+# It took more than one hour to fit with the entire dataset and it didn't work well. Tried with a shorter dataset.
+
+set.seed(14000)
+inTrain <- createDataPartition(y=dazika$NM_DISEASE, p=0.9, list=FALSE)
+training <- dazika[inTrain, ]
+testing <- dazika[-inTrain, ]
+
 
 
 # K-mean clustering using geoposition
 
-# First, I will build a dataset from the original (dazika) with the long, lat, and disease infos.
+# First, I will build a dataset from the original with the long, lat, and disease infos.
 
-df <- data.frame(long = dazika$longitude, lat = dazika$latitude, disease = dazika$NM_DISEASE)
+df <- data.frame(long = testing$longitude, lat = testing$latitude, disease = testing$NM_DISEASE)
 
 # Then, I will build a distance function using the distance algorithm distHaversine() from the geosphere package.
 
@@ -33,4 +41,5 @@ geo_dist <- function(df) {
 
 distance_matrix <- geo_dist(df)
 
-# It took more than one hour to fit and it didn't work well. Try with a shorter dataset.
+# Codes to get the k-means 
+km <- kmeans(distance_matrix,centers=3) 
