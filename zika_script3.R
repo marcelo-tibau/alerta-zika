24b67ea5ae20eade28acb0f37019db9c133b4ba5
Marcelo Tibau <marcelo@tibau.org>
2016-11-25 14:12:56 -0200

    Add files via upload

--- zika_script3.R
+++ zika_script3.R
@@ -6,55 +6,31 @@ library("rpart")
 library("rpart.plot")
 library("RColorBrewer")
 library("NbClust")
-
-
-# K-mean clustering
-
-
-dataclustplot <- function(dazika, nc=15, seed=1527){
-  dataclust <- (nrow(dazika)-1)*sum(apply(dazika,2,var))
-  for (i in 2:nc){
-    set.seed(seed)
-    dataclust[i] <- sum(kmeans(dazika, centers=i)$withinss)}
-  plot(1:nc, wss, type="b", xlab="Number of Clusters",
-       ylab="Within groups sum of squares")}
-
-dataclustplot(dazika)
-
-set.seed(1527)
-nc <- NbClust(dazika, min.nc = 2, max.nc = 15, method = "kmeans")
+library("geosphere")
 
 
 
+# K-mean clustering using geoposition
 
-inTrain <- createDataPartition(y=dazika$NM_DISEASE, p=0.7, list=FALSE)
-training <- dazika[inTrain, ]
-testing <- dazika[-inTrain, ]
+# First, I will build a dataset from the original (dazika) with the long, lat, and disease infos.
 
-# Decision Tree Algorithm
-set.seed(14000)
-modelzikaone <- rpart(NM_DISEASE~., data = training, method = "class", control = rpart.control(minsplit=30, cp=0.001))
+df <- data.frame(long = dazika$longitude, lat = dazika$latitude, disease = dazika$NM_DISEASE)
 
+# Then, I will build a distance function using the distance algorithm distHaversine() from the geosphere package.
 
+geo_dist <- function(df) {
+  require(geosphere)
+  d <- function(i, z){
+    dist <- rep(0, nrow(z))
+    dist[i:nrow(z)] <- distHaversine(z[i:nrow(z), 1:2], z[i, 1:2])
+    return(dist)
+  }
+  dm <- do.call(cbind, lapply(1:nrow(df), d, df))
+  return(as.dist(dm))
+}
 
+# Next step is to build a distance matrix
 
+distance_matrix <- geo_dist(df)
 
-modelFitDT <- rpart(classe ~., data = training, method = "class")
-fancyRpartPlot(modelFitDT)
-
-# Cross validating the model:
-predictFitDT <- predict(modelFitDT, testing, type = "class")
-
-# To check the accuracy:
-accuracy_FitDT <- confusionMatrix(predictFitDT, testing$classe)  
-accuracy_FitDT
-
-
-
-NZV_check <- nearZeroVar(training, saveMetrics = TRUE)
-training <- training[, NZV_check$nzv==FALSE]
-
-
-modelFitRF <- randomForest(NM_DISEASE~., data = training_na2)
-
-predictFitRF <- predict(modelFitRF, training_na2, type = "class")
+# It took more than one hour to fit and it didn't work well. Try with a shorter dataset.
