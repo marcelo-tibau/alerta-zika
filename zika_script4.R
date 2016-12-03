a5e9af7ef38f65cc6c89bfd51aef3e859807c771
Marcelo Tibau <marcelo@tibau.org>
2016-12-03 13:05:12 -0200

    Add files via upload

--- zika_script4.R
+++ zika_script4.R
@@ -7,6 +7,37 @@ df <- data.frame(long = dazika$longitude, lat = dazika$latitude, disease = dazik
 
 dfzika <- subset(df, disease == "Zika", select = c(long, lat))
 
+dweather <- read.csv("2016-11-07_estacao_wu.csv")
+
+weather_data <- read.csv("2016-11-07_clima_wu.csv")
+weather_RJ <- subset(weather_data, Estacao_wu_estacao_id == c("SBGL", "SBRJ"), select = c(data_dia, temp_min, temp_max, temp_med))
+
+write.csv(weather_RJ, file = "rj_weather.csv")
+
+dwrj <- read.csv("rj_weather_2016.csv")
+
+dwrj_2016 <- subset(dwrj, select = c(data_dia, temp_min, temp_max, temp_med))
+
+write.csv(dwrj_2016, file = "rj_weather_2016.csv")
+
+dat1 <- read.csv("rj_weather_2016.csv")
+
+idh_rio_reg <- read.csv("RM-63300-Rio-de-Janeiro-Base-REGIONAL-2000_2010.csv")
+
+idh_rio_reg_var <- subset(idh_rio_reg, select = c(NOME_REG, ANO, ESPVIDA, IDHM, IDHM_E, IDHM_L, IDHM_R))
+
+write.csv(idh_rio_reg_var, file = "idh_rio_reg_var.csv")
+
+idh_data <- subset(idh_rio_reg_var, ANO == 2010, select = c(NOME_REG, ANO, ESPVIDA, IDHM, IDHM_E, IDHM_L, IDHM_R))
+
+write.csv(idh_data, file = "idh_data.csv")
+
+idh_rio_coord <- read.csv("idh_excel.csv")
+
+idh_rio_coord_data <- subset(idh_rio_coord, select = c(NOME_REG, IDHM, LONGITUDE, LATITUDE))
+
+write.csv(idh_rio_coord_data, file = "idh_rio_coord_data.csv")
+
 geo_dist <- function(dfzika) {
   require(geosphere)
   d <- function(i, z){
