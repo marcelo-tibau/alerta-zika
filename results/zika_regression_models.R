506f2ece74801a25132f02bee1ed4a3c93f19e07
Marcelo Tibau <marcelo@tibau.org>
2016-12-09 19:21:14 -0200

    Add files via upload

--- results/zika_regression_models.R
+++ results/zika_regression_models.R
@@ -1,11 +1,31 @@
 dazika <- read.csv("zika_data.csv")
 
-dazika$ID_AGRAVO <- factor(dazika$ID_AGRAVO)
-dazika$NM_DISEASE <- factor(dazika$NM_DISEASE)
-dazika$DT_TRANSSM <- factor(dazika$DT_TRANSSM)
+data_cases <- read.csv("cases_per_neighborhood.csv")
 
-model1 <- lm(NM_DISEASE ~ ID_AGRAVO, data = dazika)
+dataframe_cases <- data.frame(Bairro = dazika$NM_BAIRRO, Doenca = dazika$NM_DISEASE, Dia = dazika$DT_NOTIFIC, Populacao = dazika$DT_OBITO)
+
+write.csv(dataframe_cases, file = "dataframe_cases.csv")
+
+dataframe_cases2 <- read.csv("dataframe_cases2.csv")
+
+
+# codes to drop the selected variables
+dazika$DT_TRANSRM <- dazika$DT_TRANSRS <- dazika$DT_TRANSSE <- dazika$NU_LOTE_H <- dazika$MIGRADO_W <- NULL
+
+# Modelling regressions:
+model1 <- lm(doenca ~ bairro, data = data_cases)
 summary(coef(model1))
+str(summary(model1))
 
 model2 <- lm(NM_DISEASE ~ DT_TRANSSM, data = dazika)
 summary(coef(model2))
+
+model3 <- lm(doenca ~ ., data = data_cases)
+summary(coef(model3))
+str(summary(model3))
+
+best_model <- step(model3, direction = "both")
+m3 <- sparse.model.matrix(lm(NM_DISEASE ~ ., data = dazika))
+s <- sparseVector(lm(NM_DISEASE ~ ., data = dazika), length = 10)
+
+summary(best_model)
